2da8cb5d22b481edaae2d0505ca6d887f3ec5f3a
Felix Strobel <mail@felixstrobel.de>
2019-12-20 16:40:47 +0100

    add project folder with main for 7

---
+++ uebung7/fileReceiver/src/de/gruppe1/Main.java
@@ -0,0 +1,13 @@
+package de.gruppe1;
+
+/**
+ * Wird ohne Parameter aufgerufen und wartet auf eingehende
+ * Dateiübertragungen. Empfangene Dateien werden lokal gespeichert. Nach dem Empfang einer
+ * Datei bleibt das Programm weiter aktiv und wartet auf die nächste eingehende Datenverbindung
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ uebung7/fileSender/src/de/gruppe1/Main.java
@@ -0,0 +1,13 @@
+package de.gruppe1;
+
+/**
+ * Wird mit Dateinamen und Namen des Zielrechners als Parameter aufgerufen und
+ * versendet die angegebene Datei zuverlässig an den angegebenen Zielrechner.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
